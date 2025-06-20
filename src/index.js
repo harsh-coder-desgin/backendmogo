212c4af45a463246c6e2951a76a2c0df6b6614df
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-20 15:18:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
 // require('dotenv').config({path:'/env'})
-// import { app } from "./app.js"
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import userRouter from "./routes/user.routes.js"
+import { app } from "./app.js"
+// import cors from 'cors';
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
-
-const app = express()
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-
-
-app.use("/api/v1/users",userRouter)
-
-
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-
-
-
+console.log("second");
 
 dotenv.config({
     path:'./.env'
 }) 
 
+// app.use(cors({
+//   origin: 'https://aurcode.vercel.app', // or ['http://localhost:5173', 'https://your-frontend.vercel.app']
+//   credentials: true, // only if using cookies
+// }));
 
 connectDB()
 .then(()=>{
@@ -46,58 +28,3 @@ connectDB()
 .catch((err)=>{
     console.log("mongo db connection failed",err)
 })
-
-export { app }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express"
-// const app= express()
-
-// ( async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error)=>{
-//             console.log("error",error);
-//             throw error
-//         })
-        
-//         app.listen(process.env.PORT,()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
-//     } catch (error) {
-//         console.error("error",error)
-//         throw err
-        
-//     }
-// })()
