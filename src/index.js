a44660fb032c05aa294db5a6078712381d15f0c1
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-20 13:29:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 // require('dotenv').config({path:'/env'})
-import { app } from "./app.js"
-import cors from 'cors';
-
-
-
-
-
-
-
-
-
-
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = express()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 dotenv.config({
     path:'./.env'
 }) 
 
-// app.use(cors({
-//   origin: 'https://aurcode.vercel.app', // or ['http://localhost:5173', 'https://your-frontend.vercel.app']
-//   credentials: true, // only if using cookies
-// }));
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
@@ -40,56 +34,6 @@ connectDB()
     console.log("mongo db connection failed",err)
 })
 
+import userRouter from "./routes/user.routes.js"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express"
-// const app= express()
-
-// ( async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error)=>{
-//             console.log("error",error);
-//             throw error
-//         })
-        
-//         app.listen(process.env.PORT,()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
-//     } catch (error) {
-//         console.error("error",error)
-//         throw err
-        
-//     }
-// })()
+app.use("/api/v1/users",userRouter)
